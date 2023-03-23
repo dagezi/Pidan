255730f272c9f2ce1dfccf19c5242df851f2d85e
dagezi <dagezi@gmail.com>
2023-03-23 19:51:56 +0900

    - Catch control characters. - Add hook to observe IMInputController Protocol. - Stub Romazi converter

--- IMKitSample/IMKitSampleInputController.swift
+++ IMKitSample/IMKitSampleInputController.swift
@@ -10,13 +10,48 @@ import InputMethodKit
 
 @objc(IMKitSampleInputController)
 class IMKitSampleInputController: IMKInputController {
-    override func inputText(_ string: String!, client sender: Any!) -> Bool {
-        NSLog(string)
+    let romanConverter = RomanConverter()
+    var markedText = "";
+    
+    override func updateComposition() {
+        NSLog("updateComposision")
+        super.updateComposition()
+    }
+    
+    override func cancelComposition() {
+        NSLog("cancelComposition")
+        super.cancelComposition()
+    }
+
+    override func selectionRange() -> NSRange {
+        NSLog("SelectRange")
+        return super.selectionRange()
+    }
+
+    override func replacementRange() -> NSRange {
+        NSLog("replacementRange")
+        return super.replacementRange()
+    }
+    
+    override func inputText(_ string: String!, key keyCode: Int, modifiers flags: Int, client sender: Any!) -> Bool {
+        NSLog("\(string ?? "-")/\(keyCode)/\(flags)")
         // get client to insert
         guard let client = sender as? IMKTextInput else {
             return false
         }
-        client.insertText(string+string, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+        if string[string.startIndex] < " " {
+            // TODO: more generic
+            client.insertText(
+                romanConverter.convert(source: markedText),
+                replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+            markedText = ""
+        } else {
+            markedText += string
+            client.setMarkedText(
+                markedText,
+                selectionRange: NSRange(location: 0, length: markedText.count),
+                replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+        }
         return true
     }
 }
---
+++ IMKitSample/RomanConverter.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Class to convert ASCII character stream into Japanese Hiragana/Katakana
+
+class RomanConverter {
+    func convert(source: String) -> String {
+        if (source == "a") {
+            return "あ"
+        }
+        return source
+    }
+}
