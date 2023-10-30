546636ec2f24334d2e45b6a485f6131cda9583a3
dagezi <dagezi@gmail.com>
2023-10-29 22:01:27 -0700

    many suffix types

--- IMKitSample/Convert/Dictionary.swift
+++ IMKitSample/Convert/Dictionary.swift
@@ -14,17 +14,46 @@ let nounType = DictionarySuffixType([
     "は", "が", "に", "へ", "で", "から", "より", "でも", "も", "では", "の", "と",
     "って", "を", "か", "だ", "です", ""])
 
-// let aGodanType
-// let kGodanType
-// let sGodanType
-// let tGodanType
-// let nGodanType
-// let aGodanType
-// let ichidanType
-// let sahenType
-// let keiyouType
-// let keiyoudouType
-// let sahenNounType
+let aGodanType = DictionarySuffixType([
+    "わ", "い", "う", "えば", "え", "お", "っ"
+])
+
+let kGodanType = DictionarySuffixType([
+    "か", "き", "く", "けば", "け", "こ", "い"
+])
+
+let sGodanType = DictionarySuffixType([
+    "さ", "し", "す", "せば", "せ", "そ"
+])
+
+let tGodanType = DictionarySuffixType([
+    "た", "ち", "つ", "てば", "て", "と", "っ"
+])
+let nGodanType = DictionarySuffixType([
+    "な", "に", "ぬ", "ねば", "ね", "の", "ん"
+])
+let bGodanType = DictionarySuffixType([
+    "ば", "び", "ぶ", "べば", "べ", "ぼ", "ん"
+])
+let mGodanType = DictionarySuffixType([
+    "ま", "み", "む", "めば", "め", "も", "ん"
+])
+let rGodanType = DictionarySuffixType([
+    "ら", "り", "る", "れば", "れ", "ろ", "っ"
+])
+let itidanType = DictionarySuffixType([
+   "", "る", "れば", "ろ", "よ"
+])
+let sahenType = DictionarySuffixType([
+    "さ", "し", "する", "すれ", "しろ", "せよ"
+])
+let keiyouType = DictionarySuffixType([
+    "い", "く", "けれ", "かろ", "かっ"
+])
+let keiyoudouType = DictionarySuffixType([
+    "だろ", "だっ", "で", "に", "だ", "な", "なら"
+])
+
 
 class DictionaryEntry {
     let srcRoot: String
